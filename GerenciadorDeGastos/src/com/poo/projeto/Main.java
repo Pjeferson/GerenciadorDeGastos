034fe5ca8908550a35e9ece8edaacbd2701f3b54
Pjeferson <paulojeferson34@gmail.com>
2017-12-17 22:50:51 -0300

    Criando classe para testar camada DAO

--- GerenciadorDeGastos/src/com/poo/projeto/Main.java
+++
@@ -1,7 +0,0 @@
-package com.poo.projeto;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Teste");
-	}
-}
---
+++ GerenciadorDeGastos/src/com/poo/projeto/Teste.java
@@ -0,0 +1,54 @@
+package com.poo.projeto;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import com.poo.projeto.model.Gasto;
+import com.poo.projeto.model.dao.GastoDAO;
+import com.sun.javafx.binding.StringFormatter;
+
+public class Teste {
+	public static void main(String[] args) {
+		GastoDAO gastodao = new GastoDAO();
+		Gasto gasto = new Gasto();
+		SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+		String dataTeste = "01/01/2018";
+		Date data = new Date();
+		try {
+			data = fmt.parse(dataTeste);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		gasto.setData(data);
+		gasto.setDescricao("Testanndo");
+		gasto.setPreco(1.20);
+		gasto.setId(10);
+		/*
+		 * try { gastodao.excluirGasto(gasto); } catch (SQLException e1) {
+		 * e1.printStackTrace(); }
+		 */
+
+		try {
+			gastodao.salvarGasto(gasto);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+
+		try {
+			List<Gasto> gastos = gastodao.listarGastos();
+			for (Gasto gasto2 : gastos) {
+				System.out.println("ID: " + gasto2.getId());
+				System.out.println("Data: " + new SimpleDateFormat("dd/MM/yyyy").format(gasto2.getData()));
+				System.out.println("Preço: " + gasto2.getPreco());
+				System.out.println("Descrição: " + gasto2.getDescricao());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		gastodao.fecharDAO();
+
+	}
+}
