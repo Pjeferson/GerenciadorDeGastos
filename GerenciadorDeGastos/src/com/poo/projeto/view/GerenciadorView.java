7c67093e4643811824a35848747bd1771ceb5fec
Pjeferson <paulojeferson34@gmail.com>
2017-12-18 22:36:33 -0300

    Iniciando View

--- GerenciadorDeGastos/src/com/poo/projeto/view/GerenciadorView.java
+++ GerenciadorDeGastos/src/com/poo/projeto/view/GerenciadorView.java
@@ -7,9 +7,20 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import com.poo.projeto.model.GastoTableModel;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class GerenciadorView extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
+	private GastoTableModel tableModel;
 
 	/**
 	 * Launch the application.
@@ -17,6 +28,21 @@ public class GerenciadorView extends JFrame {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				try {
+					UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (InstantiationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IllegalAccessException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				try {
 					GerenciadorView frame = new GerenciadorView();
 					frame.setVisible(true);
@@ -32,11 +58,28 @@ public class GerenciadorView extends JFrame {
 	 */
 	public GerenciadorView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 640, 480);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(297, 10, 300, 230);
+		
+		table = new JTable();
+		tableModel = new GastoTableModel();
+		table.setModel(tableModel);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.getColumnModel().getColumn(0).setPreferredWidth(50);
+		table.getColumnModel().getColumn(1).setPreferredWidth(80);
+		table.getColumnModel().getColumn(2).setPreferredWidth(240);
+		tableModel.fireTableDataChanged();
+		table.setShowVerticalLines(false);
+		table.setShowHorizontalLines(false);
+		table.setShowGrid(false);
+		
+		scrollPane.setViewportView(table);
+		contentPane.add(scrollPane);
 	}
-
 }
